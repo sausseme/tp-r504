c67987955623a68202764dcd7f0ed02d1ca776b7
sausseme <emerick.saussey@univ-rouen.fr>
2023-09-08 15:45:38 +0200

    mise a jour v4

--- TP1/test.py
+++ TP1/test.py
@@ -10,16 +10,13 @@ def test_2() :
 	assert f.puissance(-2,2) == 4
 	
 def test_3() :
-	assert f.puissance(2,2) == 4
-	
-def test_4() :
 	assert f.puissance(2,-2) == 0.25
 	
-def test_5() :
+def test_4() :
 	assert f.puissance(-2,3) == -8
 	
-def test_6() :
+def test_5() :
 	assert f.puissance(-2,0) == -1
 	
-def test_7() :
+def test_6() :
 	assert f.puissance(0,0) == 1
