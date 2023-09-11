912b249b46e31c32e96cca3d5517b9f6ad32ff5d
sausseme <emerick.saussey@univ-rouen.fr>
2023-09-11 09:19:13 +0200

    mise a jour v5

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,19 @@
 def puissance(a, b):
     if a < 0 and b == 0:
         return -1
-    else:
-        return a ** b
         
+    if a == 0 and b < 0:
+        return "ERREUR math ! Retentez un autre calcul !"
+        
+    elif b == 0:
+        return 1
+    
+    else:
+        resultat = 1
+        for i in range(abs(b)):
+            resultat *= a
+        if b < 0:
+            return 1 / resultat
+        else:
+            return resultat
+
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -5,5 +5,5 @@ import fonctions as f
 while True:
 	a = int(input("Entrez un nombre pour a: "))
 	b = int(input("Entrez un nombre pour b: "))
-	res = f.puissance(a, b)
-	print(f'{a}^{b} = {res}')
+	resultat = f.puissance(a, b)
+	print(f'{a}^{b} = {resultat}')
