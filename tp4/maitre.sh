212c72ac8464c647138b2e1e53578341f6f3f554
sausseme <emerick.saussey@univ-rouen.fr>
2023-10-11 10:53:08 +0200

    modification du TP4

---
+++ tp4/maitre.sh
@@ -0,0 +1,35 @@
+./create_network.sh
+
+if [ $? != 0 ]; then
+	echo "echec de lancement du script  create_network.sh"
+	exit 666
+fi
+
+./run_mysql.sh
+
+if [ $? != 0 ]; then 
+        echo "echec de lancement du script  run_mysql.sh"
+        exit 666
+fi
+
+./filldb.sh
+
+if [ $? != 0 ]; then 
+        echo "echec de lancement du script filldb .sh"
+	./run_mysql.sh
+	./filldb.sh
+fi
+
+./build_image.sh
+
+if [ $? != 0 ]; then 
+        echo "echec de lancement du script  build_image.sh"
+        exit 666
+fi
+
+./tp4-app.sh
+
+if [ $? != 0 ]; then 
+        echo "echec de lancement du script  tp4-app.sh"
+        exit 666
+fi
---
+++ tp4/tp4-app.sh
@@ -0,0 +1,1 @@
+docker run -d --name tp-app --network net-tp4 -p 5000:5000 im-tp4
