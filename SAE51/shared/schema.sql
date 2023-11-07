f449497d6a0ae0e40b10246134f36f7cb08e6f64
sausseme <emerick.saussey@univ-rouen.fr>
2023-11-07 11:13:17 +0100

    ajout SAE51

---
+++ SAE51/shared/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `users` (
+  `id` integer,
+  `username` varchar(255),
+  `role` varchar(255),
+  `created_at` timestamp
+);
+
+CREATE TABLE `posts` (
+  `id` integer PRIMARY KEY,
+  `title` varchar(255),
+  `body` text COMMENT 'Content of the post',
+  `user_id` integer,
+  `status` ENUM ('draft', 'published', 'private'),
+  `created_at` timestamp
+);
+
