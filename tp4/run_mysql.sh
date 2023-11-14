274e9a6c8eae4804a93f3020efab3f3cab42162b
sausseme <emerick.saussey@univ-rouen.fr>
2023-11-14 09:47:29 +0100

    adjd

--- tp4/run_mysql.sh
+++ tp4/run_mysql.sh
@@ -1,7 +1,8 @@
 docker run --rm -d \
 	-p 3307:3306\
 	-v vol-sql-demo:/var/lib/mysql \
-	--name tp4-sql \
+	--name sae51-sql \
 	--env MYSQL_ROOT_PASSWORD=foo \
 	--network net-tp4 \
-	mysql
+	--mount type=bind,source=/home/user/SAE51,target=/srv/csv \
+	mysql:8.0 --local-infile=1 --secure-file-priv=''
